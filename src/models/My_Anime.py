cbe01f9e31942fae187afb045d0d70bf5f9505e9
Ray Jewell <rayjewell268@gmail.com>
2021-01-28 19:19:01 +1100

    Completed My_Anime model 2.0

--- src/models/My_Anime.py
+++ src/models/My_Anime.py
@@ -11,13 +11,13 @@ class My_Anime(db.Model):                                                       
     anime_title = db.Column(db.String())                                                                                             # anime title, string
     anime_started = db.Column(db.DateTime, default=datetime.now)                                                                     # anime started, date
     anime_finished = db.Column(db.DateTime, default=datetime.now)                                                                    # anime finished, date
-    rating /100 = db.Column(db.Integer())                                                                                      # anime rating out of 100, integer
+    rating /100 = db.Column(db.Integer())                                                                                            # anime rating out of 100, integer
     profile_id_fk = db.Column(db.Integer, db.ForeignKey("profile.id"), nullable=False)                                               # foreign key linked to Profile, integer, must be present
 
     @classmethod
-    def date_filter(cls, year, month, day, user_id):
-        sql_query = text("SELECT * FROM  My_Anime WHERE DATE(anime_started) = ':year-:month-:day' and client_id_fk=':user_id';")
-        sql_query = text("SELECT * FROM  My_Anime WHERE DATE(anime_finished) = ':year-:month-:day' and client_id_fk=':user_id';")
+    def date_filter(cls, year, month, day, user_id):                                                                                 # data filtering function
+        sql_query = text("SELECT * FROM  My_Anime WHERE DATE(anime_started) = ':year-:month-:day' and profile_id_fk=':user_id';")    # sql query, anime_started: year, month, day and profile id
+        sql_query = text("SELECT * FROM  My_Anime WHERE DATE(anime_finished) = ':year-:month-:day' and profile_id_fk=':user_id';")   # sql query, anime_finished: year, month, day and profile id
         return  db.engine.execute(sql_query, {"year":year, "month": month, "day": day, "user_id": user_id})
 
     def __repr__(self):
