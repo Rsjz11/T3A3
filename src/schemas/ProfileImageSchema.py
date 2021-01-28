83ab2fa581ed47a26edbf5e1b92e0c2aeedf14f8
Ray Jewell <rayjewell268@gmail.com>
2021-01-28 19:41:16 +1100

    ProfileImageSchema.py completed

--- src/schemas/ProfileImageSchema.py
+++ src/schemas/ProfileImageSchema.py
@@ -1,13 +1,13 @@
 from main import db, ma                                                                     # Import the serialization object from database and main
-from schemas.UserSchema import UserSchema
-from models.Profile import Profile
-from models.ProfileImage import ProfileImage
-from marshmallow.validate import Length
+from schemas.UserSchema import UserSchema                                                   # Import UserSchema
+from models.Profile import Profile                                                          # Import Profile model
+from models.ProfileImage import ProfileImage                                                # Import ProfileImage model
+from marshmallow.validate import Length                                                     # Import the length class that will allow us to validate the length of the string
 
-class ProfileImageSchema(ma.SQLAlchemyAutoSchema):
+class ProfileImageSchema(ma.SQLAlchemyAutoSchema):                                          # Generate schema automatically
     class Meta:
-        model = ProfileImage
+        model = ProfileImage                                                                # Generate schema using ProfileImage model
 
-    filename = ma.String(required=True, validate=Length(min=1))
+    filename = ma.String(required=True, validate=Length(min=1))                             # filename, a string of atleast 1 character must be present
 
 profile_image_schema = ProfileImageSchema()
