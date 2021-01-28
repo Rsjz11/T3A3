08de0ec19ceb2abfcc603205e1b83ede2d75326f
Ray Jewell <rayjewell268@gmail.com>
2021-01-28 20:50:35 +1100

    completed profile_controller

--- src/controllers/profile_controller.py
+++ src/controllers/profile_controller.py
@@ -1,77 +1,95 @@
+
+from main import db                                                    # This is the db instance created by SQLAlchemy
+from flask import Blueprint, request, jsonify, abort                   # Import flask and various sub packages
+from schemas.ProfileSchema import profile_schema, profiles_schema      # Importing the Profile Schema
+from schemas.My_AnimeSchema import My_Animes_schema, My_Anime_schema   # Importing the My_Anime Schema
+from flask_jwt_extended import jwt_required, get_jwt_identity          # Packages for authorization via JWTs
 from models.Profile import Profile                                     # Importing the Profile Model
 from models.User import User                                           # Importing the User Model
-from schemas.ProfileSchema import profile_schema, profiles_schema      # Importing the Profile Schema
-from main import db                                                    # This is the db instance created by SQLAlchemy
-from main import bcrypt                                                # Import the hasing package from main
-from services.auth_service import verify_user 
-from sqlalchemy.orm import joinedload                                  # 
-from flask_jwt_extended import jwt_required, get_jwt_identity          # Packages for authorization via JWTs
-from flask import Blueprint, request, jsonify, abort                   # Import flask and various sub packages
+from models.My_Anime import My_Anime                                   # Importing the My Anime model
+from sqlalchemy import text                                            # Importing text from SQLAlchemy
 
 profiles = Blueprint("profiles", __name__, url_prefix="/profile")      # Creating the profile blueprint 
 
-@profiles.route("/", methods=["GET"])                                  # Route for the profile index
-def profile_index():                                                   # This function will run when the route is matched
+
+@profiles.route("/", methods=["POST"])                                 
+@jwt_required                                                          # JWT token is required for this route
+@verify_user                                                           # Auth service to make sure the correct user owns this profile
+def profile_create(user):                                              
+
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return abort(401, description="Do I know you?")
+    
+    profile_fields = profile_schema.load(request.json)
+    is_profile = Profile.query.get(user.id)
+
+    if not is_profile:
+    
+        new_profile = Profile()
+        new_profile.username = profile_fields["username"]
+        new_profile.fname = profile_fields["fname"]
+        new_profile.lname = profile_fields["lname"]
+        
+        user.profile_id.append(new_profile)                           # user = defined above; client_id is linked as relationship to user: client_id = db.relationship("Client", backref=backref("users", uselist=False)) in users table
+        
+        db.session.add(new_profile)
+        db.session.commit()
+        
+        return jsonify(client_schema.dump(new_profile))
+    
+    else:
+        return abort(401, description='Profile already exists')
+
+@profiles.route("/", methods=["GET"])
+@jwt_required
+def get_profile_details():                                            # function that returns profile details
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if not user:
+        return abort(401, description="Invalid profile")
+
+    details = Profile.query.filter_by(id=user.id).first()
+
+    return jsonify(profile_schema.dump(details))
+
+@profiles.route("/", methods=["PUT", "PATCH"])
+@jwt_required
+def profile_details_update():
+    user_id = get_jwt_identity()                                    # Update a journal entry
+    user = User.query.get(user_id)
+    if not user:
+        return abort(401, description="Invalid profile")
+
+    details = Profile.query.filter_by(id=user.id)
+    detail_fields = profile_schema.load(request.json)
+    details.update(detail_fields)
+    db.session.commit()
+    return jsonify(detail_fields)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@profiles.route("/", methods=["GET"])                                  
+def profile_index():                                                   
+                                                      
     profiles = Profile.query.options(joinedload("user")).all()         # Retrieving all profiles from the db
     return jsonify(profiles_schema.dump(profiles))                     # Returning all the profiles in json
 
-@profiles.route("/", methods=["POST"])                                 # Route for the profile create
-@jwt_required                                                          # JWT token is required for this route
-@verify_user                                                           # Auth service to make sure the correct user owns this profile
-def profile_create(user):                                              # This function will run when the route is matched
-
-    if user.profile != []:                                             # If the user already has a profile
-        return abort(400, description="User already has profile")      # Return the error "Email already in use"
-
-    profile_fields = profile_schema.load(request.json)                 # Retrieving the fields from the request
-    profile = Profile.query.filter_by(username=profile_fields["username"]).first() # Query the user table with the email and return the first user
-
-    if profile:                                                        # If a user is returned 
-        return abort(400, description="username already in use")       # Return the error "Email already in use"
-
-    new_profile = Profile()                                            # Create a new profile object from the Profile model 
-    new_profile.username = profile_fields["username"]                  # Add username to the new_profile 
-    new_profile.firstname = profile_fields["firstname"]                # Add username to the new_profile 
-    new_profile.lastname = profile_fields["lastname"]                  # Add username to the new_profile 
-    new_profile.user_id = user.id                                      # Add username to the new_profile 
     
-    user.profile.append(new_profile)                                   # Add profile to the user
-    db.session.commit()                                                # Commit the DB session
-      
-    return jsonify(profile_schema.dump(new_profile))                   # Return the newly created profile
-
-@profiles.route("/<int:id>", methods=["GET"])                          # Route for the profile create
-def profile_show(id):                                                  # Auth service to make sure the correct user owns this profile
-    profile = Profile.query.get(id)                                    # Query the user table with the id then return that user
-    return jsonify(profile_schema.dump(profile))                       # Returb the profile in JSON
-    
-
-@profiles.route("/<int:id>", methods=["PUT", "PATCH"])                 # Route for the profile create
-@jwt_required                                                          # JWT token is required for this route
-@verify_user                                                           # Auth service to make sure the correct user owns this profile
-def profile_update(user, id):                             
-    
-    profile_fields = profile_schema.load(request.json)                 # Retrieving the fields from the request
-    profile = Profile.query.filter_by(id=id, user_id=user.id)          # Query the user table with the id and the user id then return the first user
-    if not profile:                                                    # If there is no profile found
-        return abort(401, description="Unauthorized to update this profile")  # Return this error
-
-    print(profile.__dict__)
-    profile.update(profile_fields)                                     # Update the fields with the data from the request
-    db.session.commit()                                                # Commit the session to the db
-    return jsonify(profile_schema.dump(profile[0]))                    # Return the recently committed profile
-
-
-@profiles.route("/<int:id>", methods=["DELETE"])                       # Route for the profile create
-@jwt_required        
-@verify_user                                                           # Auth service to make sure the correct user owns this profile
-def profile_delete(user, id):
-    profile = Profile.query.filter_by(id=id, user_id=user.id).first()  # Query the user table with the id and the user id then return the first user
-    # print(profile[0].__dict__)
-    # return("bills")
-    if not profile:                                                    # If there is any number other than 1
-        return abort(400, description="Unauthorized to update this profile") # Return this error
-    
-    db.session.delete(profile)
-    db.session.commit()                                                # Commit the session to the db
-    return jsonify(profile_schema.dump(profile))                       # Return the deleted profile
